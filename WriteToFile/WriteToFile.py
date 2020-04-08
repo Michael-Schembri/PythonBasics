78e147816b6fd1ff0a91f99873e19360df98d784
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-04-08 11:53:22 +0200

    pandas to csv quickie

---
+++ WriteToFile/WriteToFile.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
+                   'mask': ['red', 'purple'],
+                   'weapon': ['sai', 'bo staff']})
+df.to_csv('WriteToFile\out.csv', index=False)
